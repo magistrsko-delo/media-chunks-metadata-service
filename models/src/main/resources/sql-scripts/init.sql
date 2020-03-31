bbb6ec4b856793ee7fa68b743e9d2eb43ed7cd85
Uros Zoretic <uz1734@student.uni-lj.si>
2020-03-31 09:37:20 +0200

    DB code refactor

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -6,14 +6,14 @@ INSERT INTO chunk (aws_bucket_name, aws_storage_name, length) VALUES ('sample2',
 INSERT INTO chunk (aws_bucket_name, aws_storage_name, length) VALUES ('sample2', '720p_001.ts', 3.000);
 
 
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (4, '1920x1080', 1, 0);
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (4, '1920x1080', 2, 1);
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (4, '1920x1080', 3, 2);
+INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (1, '1920x1080', 1, 0);
+INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (1, '1920x1080', 2, 1);
+INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (1, '1920x1080', 3, 2);
 
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (3, '1920x1080', 4, 1);
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (3, '1920x1080', 5, 0);
+INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (2, '1920x1080', 4, 1);
+INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (2, '1920x1080', 5, 0);
 
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (4, '1280x720', 5, 0);
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (4, '1280x720', 6, 1);
+INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (2, '1280x720', 5, 0);
+INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (2, '1280x720', 6, 1);
 
 
