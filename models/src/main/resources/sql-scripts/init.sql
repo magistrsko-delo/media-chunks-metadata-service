a77050e04b561d99146ad15ad6a47ea00bbdcb50
Uros Zoretic <uz1734@student.uni-lj.si>
2020-04-21 14:55:48 +0200

    test data update

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -2,8 +2,6 @@ INSERT INTO chunk (aws_bucket_name, aws_storage_name, length) VALUES ('video-sam
 INSERT INTO chunk (aws_bucket_name, aws_storage_name, length) VALUES ('video-sample1', '1080p_001.ts', 5.000);
 INSERT INTO chunk (aws_bucket_name, aws_storage_name, length) VALUES ('video-sample1', '1080p_002.ts', 4.000);
 INSERT INTO chunk (aws_bucket_name, aws_storage_name, length) VALUES ('sample2', '1080p_000.ts', 4.030);
-INSERT INTO chunk (aws_bucket_name, aws_storage_name, length) VALUES ('sample2', '720p_000.ts', 3.000);
-INSERT INTO chunk (aws_bucket_name, aws_storage_name, length) VALUES ('sample2', '720p_001.ts', 3.000);
 
 
 INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (1, '1920x1080', 1, 0);
@@ -12,8 +10,5 @@ INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) V
 
 INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (2, '1920x1080', 4, 1);
 INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (2, '1920x1080', 5, 0);
-
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (2, '1280x720', 5, 0);
-INSERT INTO link_media_chunks (fk_media_id, resolution, fk_chunk_id, position) VALUES (2, '1280x720', 6, 1);
 
 
